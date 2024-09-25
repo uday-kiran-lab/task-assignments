819ba149cf0a30aaa33270a05d77d688be465621
Udaykiran <udaykiran.e98@gmail.com>
2024-09-25 22:12:40 +0530

    added solutions

--- repated-word-count.sh
+++ repated-word-count.sh
@@ -4,7 +4,7 @@ echo "Enter a file Name:"
 read File_Name
 if [ -f ${File_Name} ]
 then
-    file_read = cat ${File_Name} | tr -c '[:alnum:]' '[\n*]' | tr '[:upper:]' '[:lower:]' | sort | uniq -c | sort -r |head -n 5
+    file_read = $(cat ${File_Name} | tr -c '[:alnum:]' '[\n*]' | tr '[:upper:]' '[:lower:]' | sort | uniq -c | sort -r |head -n 5)
 else
     echo " file is not exists. please enter correct filename"
 fi
